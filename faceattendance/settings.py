1e05ee8e488fd84ccced1c6d66c7b0b642a85547
Subodh Kumar <subodh1042003@gmail.com>
2026-09-26 22:34:42 +0530

    Fix WhiteNoise static storage configuration

--- faceattendance/settings.py
+++ faceattendance/settings.py
@@ -108,7 +108,14 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # MEDIA FILES
 
